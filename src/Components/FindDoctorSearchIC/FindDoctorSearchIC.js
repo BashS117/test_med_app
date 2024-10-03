1a62457a395301e2c5ed2a9f726e480fd9dc4fba
Bash117 <sebasrm2000@gmail.com>
2024-10-03 17:11:23 -0400

    Create the FindDoctorSearch component

--- src/Components/FindDoctorSearchIC/FindDoctorSearchIC.js
+++ src/Components/FindDoctorSearchIC/FindDoctorSearchIC.js
@@ -29,11 +29,11 @@ const FindDoctorSearchIC = () => {
 
                         <input type="text" className="search-doctor-input-box" placeholder="Search doctors, clinics, hospitals, etc." onFocus={() => setDoctorResultHidden(false)} onBlur={() => setDoctorResultHidden(true)} value={searchDoctor} onChange={(e) => setSearchDoctor(e.target.value)} />
                         
-                        <div className="findiconimg"><img className='findIcon' src={process.env.PUBLIC_URL + '/images/search.svg'} alt=""/></div>
+                        <div className="findiconimg"><img className='findIcon' src='https://img.icons8.com/?size=100&id=132&format=png&color=000000' alt=""/></div>
                         <div className="search-doctor-input-results" hidden={doctorResultHidden}>
                             {
                                 specialities.map(speciality => <div className="search-doctor-result-item" key={speciality} onMouseDown={() => handleDoctorSelect(speciality)}>
-                                    <span><img src={process.env.PUBLIC_URL + '/images/search.svg'} alt="" style={{height:"10px", width:"10px"}} width="12" /></span>
+                                    <span><img src='https://img.icons8.com/?size=100&id=132&format=png&color=000000' alt="" style={{height:"14px", width:"14px"}} width="12" /></span>
                                     <span>{speciality}</span>
                                     <span>SPECIALITY</span>
                                 </div>)
